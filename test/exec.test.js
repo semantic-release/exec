b4462db5d425b6771bb8833445027666a81f1e34
Olabode Lawal-Shittabey <babblebey@gmail.com>
2024-11-18 13:48:18 +0100

    feat: ES Module (#411)
    * refactor: Convert require statements to ES6 imports
    
    * refactor: use `lodash-es` instead of `lodash`
    
    * refactor: remove `#readme` from `"package.json".homepage`
    
    * refactor: update "package.json" exports field for `esm`
    
    * refactor: use `createRequire` to get `homepage` from `package.json`
    
    * refactor: update `lib` functions to use named exports
    
    * refactor: update debug import in index.js
    
    * refactor: add "type" field to package.json
    
    * refactor: update import paths to `../index.js` in test files
    
    * refactor: replace `xo` with `prettier`
    
    * fix: `npm audit fix` package in lockfile
    
    * refactor: bump `semantic-release` peer-dependencies version
    
    * refactor: update Node.js versions in test workflow
    
    Update the Node.js versions in the test workflow to include 20.8.1, 20, and 21. This ensures compatibility with different versions of Node.js during testing.
    
    Also, replace the deprecated "npm ci" command with "npm clean-install" to install dependencies.
    
    Additionally, add a step to run "npm audit signatures" to check for any security issues in the dependencies.
    
    Finally, include a step to scan the lockfile for security issues using "lockfile-lint".
    
    * refactor: Update Node.js versions in test workflow
    
    * refactor: temporarily Remove `npm audit signatures` step in test workflow
    
    * fix(lint): initial prettier lint
    
    * refactor: update `semantic-release` peer-dependencies version
    
    BREAKING CHANGE: the minimum required version of semantic-release to use `@semantic-release/exec` is now v24.1.0; the warn logger method/function is now available to use in plugin
    
    * refactor: replace `nyc` with `c8` as package for code coverage
    
    * refactor: update Node.js versions in package-lock.json and package.json
    
    * refactor: update `semantic-release` peer-dependencies version
    
    * Update .github/workflows/test.yml
    
    Co-authored-by: Matt Travi <126441+travi@users.noreply.github.com>
    
    * refactor: update package.json to use fixed versions for `prettier` and `semantic-release`
    
    * refactor: add main entry point to package.json
    
    BREAKING CHANGE: `@semantic-release/exec` is now a native ES Module. It has named exports for each plugin hook (verifyConditions, analyzeCommits, verifyRelease, generateNotes, prepare, publish, addChannel, success, fail)
    
    ---------
    
    Co-authored-by: Matt Travi <126441+travi@users.noreply.github.com>

--- test/exec.test.js
+++ test/exec.test.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const test = require('ava');
-const {stub} = require('sinon');
-const {WritableStreamBuffer} = require('stream-buffers');
-const exec = require('../lib/exec.js');
+import { resolve } from "path";
+import test from "ava";
+import { stub } from "sinon";
+import { WritableStreamBuffer } from "stream-buffers";
+import exec from "../lib/exec.js";
 
 test.beforeEach((t) => {
   t.context.stdout = new WritableStreamBuffer();
@@ -10,60 +10,92 @@ test.beforeEach((t) => {
   // Mock logger
   t.context.log = stub();
   t.context.error = stub();
-  t.context.logger = {log: t.context.log, error: t.context.error};
+  t.context.logger = {
+    log: t.context.log,
+    error: t.context.error,
+    warn: t.context.warn,
+  };
 });
 
-test('Pipe script output to stdout and stderr', async (t) => {
-  const pluginConfig = {publishCmd: '>&2 echo "write to stderr" && echo "write to stdout"'};
-  const context = {stdout: t.context.stdout, stderr: t.context.stderr, logger: t.context.logger, options: {}};
-
-  const result = await exec('publishCmd', pluginConfig, context);
-
-  t.is(result, 'write to stdout');
-  t.is(t.context.stdout.getContentsAsString('utf8').trim(), 'write to stdout');
-  t.is(t.context.stderr.getContentsAsString('utf8').trim(), 'write to stderr');
-});
-
-test('Generate command with template', async (t) => {
+test("Pipe script output to stdout and stderr", async (t) => {
   const pluginConfig = {
-    publishCmd: `./test/fixtures/echo-args.sh \${config.conf} \${lastRelease.version}`,
-    conf: 'confValue',
+    publishCmd: '>&2 echo "write to stderr" && echo "write to stdout"',
   };
   const context = {
     stdout: t.context.stdout,
     stderr: t.context.stderr,
-    lastRelease: {version: '1.0.0'},
+    logger: t.context.logger,
+    options: {},
+  };
+
+  const result = await exec("publishCmd", pluginConfig, context);
+
+  t.is(result, "write to stdout");
+  t.is(t.context.stdout.getContentsAsString("utf8").trim(), "write to stdout");
+  t.is(t.context.stderr.getContentsAsString("utf8").trim(), "write to stderr");
+});
+
+test("Generate command with template", async (t) => {
+  const pluginConfig = {
+    publishCmd: `./test/fixtures/echo-args.sh \${config.conf} \${lastRelease.version}`,
+    conf: "confValue",
+  };
+  const context = {
+    stdout: t.context.stdout,
+    stderr: t.context.stderr,
+    lastRelease: { version: "1.0.0" },
     logger: t.context.logger,
   };
 
-  const result = await exec('publishCmd', pluginConfig, context);
-  t.is(result, 'confValue 1.0.0');
+  const result = await exec("publishCmd", pluginConfig, context);
+  t.is(result, "confValue 1.0.0");
 });
 
 test('Execute the script with the specified "shell"', async (t) => {
-  const context = {stdout: t.context.stdout, stderr: t.context.stderr, logger: t.context.logger};
+  const context = {
+    stdout: t.context.stdout,
+    stderr: t.context.stderr,
+    logger: t.context.logger,
+  };
 
-  let result = await exec('publishCmd', {publishCmd: 'echo $0', shell: 'bash'}, context);
-  t.is(result, 'bash');
+  let result = await exec(
+    "publishCmd",
+    { publishCmd: "echo $0", shell: "bash" },
+    context,
+  );
+  t.is(result, "bash");
 
-  result = await exec('publishCmd', {publishCmd: 'echo $0', shell: 'sh'}, context);
-  t.is(result, 'sh');
+  result = await exec(
+    "publishCmd",
+    { publishCmd: "echo $0", shell: "sh" },
+    context,
+  );
+  t.is(result, "sh");
 });
 
 test('Execute the script in "cmd" if no step specific command is passed', async (t) => {
-  const context = {stdout: t.context.stdout, stderr: t.context.stderr, logger: t.context.logger};
+  const context = {
+    stdout: t.context.stdout,
+    stderr: t.context.stderr,
+    logger: t.context.logger,
+  };
 
-  const result = await exec('publishCmd', {cmd: 'echo run cmd'}, context);
-  t.is(result, 'run cmd');
+  const result = await exec("publishCmd", { cmd: "echo run cmd" }, context);
+  t.is(result, "run cmd");
 });
 
 test('Exececute the script in cmd from the relative in "execCwd"', async (t) => {
   const pluginConfig = {
     publishCmd: `./fixtures/echo-args.sh $PWD`,
-    execCwd: 'test',
+    execCwd: "test",
   };
-  const context = {stdout: t.context.stdout, stderr: t.context.stderr, logger: t.context.logger, cwd: process.cwd()};
+  const context = {
+    stdout: t.context.stdout,
+    stderr: t.context.stderr,
+    logger: t.context.logger,
+    cwd: process.cwd(),
+  };
 
-  const result = await exec('publishCmd', pluginConfig, context);
-  t.is(result, path.resolve(process.cwd(), 'test'));
+  const result = await exec("publishCmd", pluginConfig, context);
+  t.is(result, resolve(process.cwd(), "test"));
 });
